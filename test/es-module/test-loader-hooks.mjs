faa4bb49403657e806e2bd84b28b0f7dde43f9c0
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2024-04-28 13:28:36 +0200

    test: add more cases & de-dupe

--- test/es-module/test-loader-hooks.mjs
+++ test/es-module/test-loader-hooks.mjs
@@ -10,41 +10,95 @@ import hooksModule from 'internal/modules/esm/hooks';
 
 const { Chain, Hook, Hooks } = hooksModule;
 
-console.log('hooksModule:', hooksModule);
-console.log('Chain:', Chain);
-
 describe('Loader Hooks', { concurrency: !process.env.TEST_PARALLEL }, () => {
   describe('Hooks', { concurrency: !process.env.TEST_PARALLEL }, () => {
     it.todo('should initialise with default hooks');
   });
 
   describe('Chain', { concurrency: !process.env.TEST_PARALLEL }, () => {
+    const type = 'resolve';
+    function firstResolve() {}
+    const urlResolve1 = 'test/resolve/1';
+    function secondResolve() {}
+    const urlResolve2 = 'test/resolve/2';
+    function thirdResolve() {}
+    const urlResolve3 = 'test/resolve/3';
+
     it('should instantiate with provided options', () => {
-      function resolve() {}
-      const type = 'resolve';
       const chain = new Chain(type, () => {}, {
-        fn: resolve,
-        url: 'test/resolve',
+        fn: firstResolve,
+        url: urlResolve1,
       });
 
-      assert.equal(chain.end.fn, resolve);
+      assert.equal(chain.type, type);
+      assert.equal(chain.end.fn, firstResolve);
       assert.equal(chain.end.type, type);
       assert.equal(chain.end.next, undefined);
       assert.equal(chain.end.prev, undefined);
-      assert.equal(chain.type, type);
+    });
+
+    it('should instantiate with provided options (multiple initial hooks)', () => {
+      const chain = new Chain(type, () => {}, {
+        fn: firstResolve,
+        url: urlResolve1,
+      }, {
+        fn: secondResolve,
+        url: urlResolve2,
+      });
+
+      assert.equal(chain.end.type, type);
+      assert.equal(chain.end.fn, secondResolve);
+
+      assert.equal(chain.end.next.fn, firstResolve);
+      assert.equal(chain.end.next.type, type);
+      assert.equal(chain.end.next.prev.fn, secondResolve);
+
+      assert.equal(chain.end.prev, undefined);
     });
 
     describe('append()', () => {
       it('should add a new item to the chain', () => {
-        function resolve() {}
-        const type = 'resolve';
         const chain = new Chain(type, () => {}, {
-          fn: resolve,
-          url: 'test/resolve',
+          fn: firstResolve,
+          url: urlResolve1,
         });
 
-        assert.equal(chain.end.fn, resolve);
-        assert.equal(chain.type, type);
+        assert.equal(chain.end.fn, firstResolve);
+
+        chain.append({
+          fn: secondResolve,
+          url: urlResolve2,
+        });
+
+        assert.equal(chain.end.prev, undefined);
+        assert.equal(chain.end.fn, secondResolve);
+
+        assert.equal(chain.end.next.fn, firstResolve);
+        assert.equal(chain.end.next.prev.fn, secondResolve);
+      });
+
+      it('should add a new item to a long chain', () => {
+        const chain = new Chain(type, () => {}, {
+          fn: firstResolve,
+          url: urlResolve1,
+        }, {
+          fn: secondResolve,
+          url: urlResolve2,
+        });
+
+        chain.append({
+          fn: thirdResolve,
+          url: urlResolve3,
+        });
+
+        assert.equal(chain.end.fn, thirdResolve);
+        assert.equal(chain.end.prev, undefined);
+
+        assert.equal(chain.end.next.fn, secondResolve);
+        assert.equal(chain.end.next.prev.fn, thirdResolve);
+
+        assert.equal(chain.end.next.next.fn, firstResolve);
+        assert.equal(chain.end.next.next.prev.fn, secondResolve);
       });
     });
   });
